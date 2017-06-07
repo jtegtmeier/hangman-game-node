4d1a6640b7de8fa3c25b75265eddcfacb5498e7c
jtegtmeier <jcbtegtmeier@gmail.com>
2017-06-07 12:22:03 -0700

    added placeholder elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import ReactSVG from 'react-svg'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="header">
           <h2>Hangman Game</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="intro">
+          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+          <p>There are some placeholder elements setup that need implementation.</p>
+        </div>
+        <div className="main">
+          <ReactSVG path="hangman.svg"/>
+          <div className="game-controls">
+            <button>New Game</button>
+            <div className="remaining-alphabet">ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
+            <input type="text" placeholder="Guess a letter..."/>
+            <div className="puzzle-word">_ _ _ _ _ _   _ _ _ _</div>
+          </div>
+        </div>
       </div>
     );
   }
